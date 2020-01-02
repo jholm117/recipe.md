978d6852210a51fa5097ef6951b75cf92c0621b1
jholm117 <jholm117@gmail.com>
2020-01-01 18:38:56 -0600

    Updates

---
+++ precache-manifest.3496313bbf230d7a3426b5bae1d7ee96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ae6ccfe6f83b06520d5b2e85d58427",
+    "url": "/recipe.md/index.html"
+  },
+  {
+    "revision": "8ea90fb7d44a895512a0",
+    "url": "/recipe.md/static/css/main.d6d28274.chunk.css"
+  },
+  {
+    "revision": "ff41d0b53dbd470e0cde",
+    "url": "/recipe.md/static/js/2.b85dbb89.chunk.js"
+  },
+  {
+    "revision": "ccbc1d8768028cb10e33433b5c30a082",
+    "url": "/recipe.md/static/js/2.b85dbb89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ea90fb7d44a895512a0",
+    "url": "/recipe.md/static/js/main.22d593af.chunk.js"
+  },
+  {
+    "revision": "5c3e567afa3a0465fc44",
+    "url": "/recipe.md/static/js/runtime-main.3d3ef90f.js"
+  }
+]);
--- profile/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var allRecipes_1 = require("./allRecipes");
-exports.AllRecipes = allRecipes_1.AllRecipes;
-var cookieandkate_1 = require("./cookieandkate");
-exports.CookieAndKate = cookieandkate_1.CookieAndKate;
-var theendlessmeal_1 = require("./theendlessmeal");
-exports.TheEndlessMeal = theendlessmeal_1.TheEndlessMeal;
