551791cf42248277eef02ed3add384d9d24d10a4
Jeff Holm <jholm117@gmail.com>
2020-12-05 08:16:58 -0800

    format parser

--- ui/src/parser/index.ts
+++ ui/src/parser/index.ts
@@ -1,25 +1,26 @@
-import * as cheerio from 'cheerio';
-import { AllRecipes, CookieAndKate } from './profile';
-import { TheSpruceEats } from './profile/thespruceeats';
-import { Profile } from './profile/profile';
+import * as cheerio from "cheerio";
+import { AllRecipes, CookieAndKate } from "./profile";
+import { TheSpruceEats } from "./profile/thespruceeats";
+import { Profile } from "./profile/profile";
 
 export function markdown(recipe: Recipe) {
-
-  const s = `# ${recipe.title}\n\n## Ingredients\n\n${ul(recipe.ingredients)}\n## Directions\n\n${ol(recipe.directions)}`;
+  const s = `# ${recipe.title}\n\n## Ingredients\n\n${ul(
+    recipe.ingredients
+  )}\n## Directions\n\n${ol(recipe.directions)}`;
 
   return s;
 }
 
-function ul(lst: string[]){
-  return lstToString(lst.map(el => `- ${el}`));
+function ul(lst: string[]) {
+  return lstToString(lst.map((el) => `- ${el}`));
 }
 
-function ol(lst: string[]){
-  return lstToString(lst.map((el, index) => `${index+1}. ${el}`));
+function ol(lst: string[]) {
+  return lstToString(lst.map((el, index) => `${index + 1}. ${el}`));
 }
 
-function lstToString(lst: string[]){
-  return lst.map(el => `${el}\n\n`).join('');
+function lstToString(lst: string[]) {
+  return lst.map((el) => `${el}\n\n`).join("");
 }
 
 export const profiles: Profile[] = [
@@ -35,14 +36,16 @@ export interface Recipe {
 }
 
 export async function readInput(url: string): Promise<Recipe | null> {
-  const response = await fetch(url);
+  const response = await fetch(url, { mode: "no-cors" });
   const text = await response.text();
   const $ = cheerio.load(text);
-  const chosenProfile = profiles.find((profile) => url.includes(profile.domain));
-  if(!chosenProfile){
-    console.error('url not supported', chosenProfile);
+  const chosenProfile = profiles.find((profile) =>
+    url.includes(profile.domain)
+  );
+  if (!chosenProfile) {
+    console.error("url not supported", chosenProfile);
     return null;
-  } 
+  }
 
   const recipe: Recipe = {
     title: chosenProfile.title($),
