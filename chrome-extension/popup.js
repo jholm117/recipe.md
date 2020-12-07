c70c07217a1a01c77be0b1401ccc8468d7bf15fd
Jeff Holm <jholm117@gmail.com>
2020-12-06 18:45:52 -0800

    chrome: update redirect url

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,20 +1,14 @@
 let changeColor = document.getElementById("changeColor");
 
-// chrome.storage.sync.get("color", function (data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute("value", data.color);
-// });
-
 changeColor.onclick = function (element) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     // chrome.tabs.executeScript(tabs[0].id, {
     //   code: 'document.body.style.backgroundColor = "' + color + '";',
     // });
     const activeTab = tabs[0];
-    const newUrl = `https://jholm117.github.io/recipe.md/#${activeTab.url}`;
+    const newUrl = `https://recipe-md.azurewebsites.net/recipe/${activeTab.url.replace("https://", "")}`;
     chrome.tabs.executeScript(activeTab.id, {
       code: `window.location.href="${newUrl}"`,
     });
-    // chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
   });
 };
