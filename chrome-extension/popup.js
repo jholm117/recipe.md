4e1a1c8de4ed2bc2fbb373a8a81f51ad77b26e75
Jeff Holm <jholm117@gmail.com>
2020-12-15 19:19:00 -0800

    chrome-extension: format

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,11 +2,11 @@ let changeColor = document.getElementById("changeColor");
 
 changeColor.onclick = function (element) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    // chrome.tabs.executeScript(tabs[0].id, {
-    //   code: 'document.body.style.backgroundColor = "' + color + '";',
-    // });
     const activeTab = tabs[0];
-    const newUrl = `https://recipe-md.azurewebsites.net/recipe/${activeTab.url.replace("https://", "")}`;
+    const newUrl = `https://recipe-md.azurewebsites.net/recipe/${activeTab.url.replace(
+      "https://",
+      ""
+    )}`;
     chrome.tabs.executeScript(activeTab.id, {
       code: `window.location.href="${newUrl}"`,
     });
